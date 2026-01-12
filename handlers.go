4ce2364e3ad4874cc10fef3a456a9b007257254f
UkaSystems <t.dillais@gmail.com>
2026-01-12 14:12:53 +0100

    clears 4-2 json exercise

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -36,4 +37,50 @@ func (cfg *apiConfig) resetHitCounter(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits counter reset to 0\n"))
+}
+
+func (cfg *apiConfig) validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		Body string `json:"body"`
+	}
+
+	type chirpValidationResponse struct {
+		Error string `json:"error"`
+		Valid bool   `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	c := chirp{}
+	validity := false
+	errResponse := ""
+	statusCode := http.StatusOK
+
+	err := decoder.Decode(&c)
+	if err != nil {
+		errResponse = fmt.Sprintf("Something went wrong: %v", err)
+		statusCode = http.StatusBadRequest
+	}
+
+	if len(c.Body) <= 140 {
+		validity = true
+	} else {
+		validity = false
+		errResponse = "Chirp is too long"
+		statusCode = http.StatusBadRequest
+	}
+
+	chirpResponse := chirpValidationResponse{
+		Valid: validity,
+		Error: errResponse,
+	}
+
+	dat, err := json.Marshal(chirpResponse)
+	if err != nil {
+		fmt.Printf("Error marshaling response: %v", err)
+		statusCode = http.StatusInternalServerError
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(dat)
 }
--- main.go
+++ main.go
@@ -33,6 +33,7 @@ func main() {
 	serveMux.Handle("GET /api/metrics", http.HandlerFunc(apiCfg.logRequestsNum))
 	serveMux.Handle("GET /admin/metrics", http.HandlerFunc(apiCfg.adminMetricsHandler))
 	serveMux.Handle("POST /admin/reset", http.HandlerFunc(apiCfg.resetHitCounter))
+	serveMux.Handle("POST /api/validate_chirp", http.HandlerFunc(apiCfg.validateChirpHandler))
 
 	var server = &http.Server{
 		Addr:    ":8080",
