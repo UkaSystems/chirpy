308445622204f39d3b5d86775b16503d305eaef2
UkaSystems <t.dillais@gmail.com>
2026-01-12 08:58:30 +0100

    clears 2-3 'Routing'

--- handlers.go
+++ handlers.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func readinessHandler(writer http.ResponseWriter, req *http.Request) {
-	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte(http.StatusText(http.StatusOK)))
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) logRequestsNum(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Hits: %v\n", cfg.fileserverHits.Load())
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %v\n", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) resetHitCounter(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits counter reset to 0\n"))
 }
--- main.go
+++ main.go
@@ -3,17 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	var serveMux = http.NewServeMux()
 
 	// serve static files from the current directory under /app/:
 	var fileServer = http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-	serveMux.Handle("/app/", fileServer)
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
 
 	// readiness probe endpoint:
-	serveMux.Handle("/healthz", http.HandlerFunc(readinessHandler))
+	serveMux.Handle("GET /healthz", http.HandlerFunc(readinessHandler))
+	serveMux.Handle("GET /metrics", http.HandlerFunc(apiCfg.logRequestsNum))
+	serveMux.Handle("POST /reset", http.HandlerFunc(apiCfg.resetHitCounter))
 
 	var server = &http.Server{
 		Addr:    ":8080",
